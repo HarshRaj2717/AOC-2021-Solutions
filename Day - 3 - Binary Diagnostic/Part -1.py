6a15c3229c21908af150eb12abf09b99434507bc
Harsh Raj <harshraj2717@gmail.com>
2022-03-11 20:07:38 +0530

    Day - 3 - Part - 1

---
+++ Day - 3 - Binary Diagnostic/Part -1.py
@@ -0,0 +1,24 @@
+from input import input_text
+
+
+def main():
+    gamma = ''
+    epsilon = ''
+    for i in range(len(input_text[0])):
+        list1 = []
+
+        for j in range(len(input_text)):
+            list1.append(input_text[j][i])
+
+        if list1.count('0') > list1.count('1'):
+            gamma += '0'
+            epsilon += '1'
+        else:
+            gamma += '1'
+            epsilon += '0'
+
+    print(int(gamma, 2)*int(epsilon, 2))
+
+
+if __name__ == "__main__":
+    main()
